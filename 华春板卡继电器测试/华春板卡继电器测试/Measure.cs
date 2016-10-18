65e1244664b0c1e519cb4ace20a7d51d64bd8cbc
wyf <hitwangyanfei@163.com>
2016-10-18 14:35:51 +0800

    把IMeasure接口改为Measure基类

--- 华春板卡继电器测试/华春板卡继电器测试/Measure.cs
+++ 华春板卡继电器测试/华春板卡继电器测试/Measure.cs
@@ -5,8 +5,11 @@ using System.Text;
 
 namespace 华春板卡继电器测试
 {
-    class IMeasure
+    class Measure
     {
+
+        public string name { get; set; }
+
         public void mesure()
         {
             
