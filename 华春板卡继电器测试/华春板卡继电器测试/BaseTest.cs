bc85739b92fb51844df8375c201ea97be39142a4
wyf <hitwangyanfei@163.com>
2016-10-18 14:26:18 +0800

    添加BaseTest测试基类

---
+++ 华春板卡继电器测试/华春板卡继电器测试/BaseTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 华春板卡继电器测试
+{
+    public class BaseTest
+    {
+        public BaseTest() { }
+
+        public void Test()
+        {
+            
+        }
+    }
+}
