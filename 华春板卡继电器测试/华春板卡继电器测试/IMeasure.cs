c8c677905f538bda427a7f2a55cca8b159b2fd7a
wyf <hitwangyanfei@163.com>
2016-10-18 14:26:58 +0800

    添加IMeasure接口

---
+++ 华春板卡继电器测试/华春板卡继电器测试/IMeasure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 华春板卡继电器测试
+{
+    class IMeasure
+    {
+        public void mesure()
+        {
+            
+        }
+    }
+}
